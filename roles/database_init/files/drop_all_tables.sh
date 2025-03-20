5f26783b5c6195cd9c8a867ba69bd96b9812652c
durist <durist@ucar.edu>
2025-03-20 13:23:18 -0600

    Fixed credentials

--- roles/database_init/files/drop_all_tables.sh
+++ roles/database_init/files/drop_all_tables.sh
@@ -7,13 +7,12 @@ homedir=/var/atlassian/application-data/confluence
 mycnf=$(/usr/local/bin/create_mycnf.sh)
 
 script=$(mktemp)
-echo 'use confluence;' >> $script
-echo 'SET FOREIGN_KEY_CHECKS = 0;' >> $script
-for t in $(mysql --defaults-file=/tmp/mycnf -sre "show tables;"); do
+echo 'SET FOREIGN_KEY_CHECKS = 0;' > $script
+for t in $(mysql --defaults-file=$mycnf -sre "show tables;"); do
         echo "DROP TABLE IF EXISTS \`$t\`;" >> $script
 done
 echo 'SET FOREIGN_KEY_CHECKS = 1;' >> $script
 
-mysql --defaults-file=/tmp/mycnf < $script
+mysql --defaults-file=$mycnf < $script
 rm $script
 rm $mycnf
