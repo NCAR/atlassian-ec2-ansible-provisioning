d0d43103abed2578fab7d714ed6635d8fafd799f
durist <durist@ucar.edu>
2025-04-25 13:07:03 -0600

    Create database and user from ansible

--- roles/database_init/files/create_mycnf.sh
+++ roles/database_init/files/create_mycnf.sh
@@ -9,14 +9,14 @@
 # Extract the relevant env vars from cloud-init.
 # We do this convoluted temp file thing to get around quoting issues.
 set_vars=$(mktemp)
-for var in 'ATL_DB_CREDENTIAL_SECRET_ARN' 'ATL_DB_HOST' 'ATL_JDBC_DB_NAME'; do
+for var in 'ATL_DBUSER_CREDENTIAL_SECRET_ARN' 'ATL_DB_HOST' 'ATL_JDBC_DB_NAME'; do
     echo $(grep "export $var" /var/lib/cloud/instance/user-data.txt) >> $set_vars
 done
 . $set_vars
 rm $set_vars
 
-dbuser=$(aws secretsmanager get-secret-value --secret-id "$ATL_DB_CREDENTIAL_SECRET_ARN" --output text | awk '{print $4}' | jq -r .user)
-dbpassword=$(aws secretsmanager get-secret-value --secret-id "$ATL_DB_CREDENTIAL_SECRET_ARN" --output text | awk '{print $4}' | jq -r .password)
+dbuser=$(aws secretsmanager get-secret-value --secret-id "$ATL_DBUSER_CREDENTIAL_SECRET_ARN" --output text | awk '{print $4}' | jq -r .user)
+dbpassword=$(aws secretsmanager get-secret-value --secret-id "$ATL_DBUSER_CREDENTIAL_SECRET_ARN" --output text | awk '{print $4}' | jq -r .password)
 
 mycnf=$(mktemp)
 cat <<MYCNF >$mycnf
