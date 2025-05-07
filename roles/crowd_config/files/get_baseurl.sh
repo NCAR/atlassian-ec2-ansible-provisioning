5b0ca838829db3ee01c875d037cfc3e0ce239aa7
durist <durist@ucar.edu>
2025-05-07 14:49:50 -0600

    Added get_baseurl.sh script

--- roles/crowd_config/files/get_baseurl.sh
+++ roles/crowd_config/files/get_baseurl.sh
@@ -1,4 +1,8 @@
+# Get the base url from the database--
+# used on clone restores to get the clone source base url
+#
 # This is app-specific
+#
 
 mycnf=$(/usr/local/bin/create_mycnf.sh)
 echo $(mysql --defaults-file=$mycnf --batch -Ne "select property_value from cwd_property where property_name = 'base.url';")
